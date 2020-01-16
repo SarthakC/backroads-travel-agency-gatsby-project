f7e6f974f2c665de54c2ce4a52469b3891719c3f
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-16 21:53:27 +0530

    initial commit

---
+++ src/pages/404.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const error = () => {
+  return <div>Hello from error page</div>
+}
+
+export default error
---
+++ src/pages/tours.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const tours = () => {
+  return <div>Hello from tours page.</div>
+}
+
+export default tours
