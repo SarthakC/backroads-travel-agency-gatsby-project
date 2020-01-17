3498219d9a5feba0ba8508efb8f1b7b51a39a0a0
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-17 20:04:35 +0530

    [added] about section

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -1,12 +1,38 @@
 import React from "react"
 import Title from "../../Title/Title"
+import styles from "./About.module.css"
+import img from "../../../images/defaultBcg.jpeg"
 
 const About = () => {
   return (
-    <div>
-      Hello from about.
+    <section className={styles.about}>
       <Title title="about" subtitle="us" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>explore the difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget
+            justo quis nunc interdum aliquet quis sit amet leo. Integer
+            hendrerit nibh in nisi convallis aliquet. In hac habitasse platea
+            dictumst.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget
+            justo quis nunc interdum aliquet quis sit amet leo. Integer
+            hendrerit nibh in nisi convallis aliquet. In hac habitasse platea
+            dictumst.
+          </p>
+          <button className="btn-primary" type="button">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
