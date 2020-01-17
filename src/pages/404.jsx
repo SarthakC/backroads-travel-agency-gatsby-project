316737040411584945bb0d9f2c452ea04f4ae970
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-17 18:51:37 +0530

    [added] 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
+
 import Layout from "../components/Layout/Layout"
+import styles from "../css/error.module.css"
+import Banner from "../components/Banner/Banner.jsx"
 
 const error = () => {
-  return <Layout>Hello from error page</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="btn-white">
+            Back to home page
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
 
 export default error
