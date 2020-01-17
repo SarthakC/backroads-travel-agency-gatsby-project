554b9f91d3ccfdb48330888418c35e80f18b7fb2
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-17 14:01:50 +0530

    [added] links and social icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import { faAdobe } from "react-icons/fa"
+import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
     <div>
       <h1>I am navbar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/rooms">Rooms</Link>
     </div>
   )
 }
---
+++ src/constants/social-icons.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitterSquare, FaSquareSpace } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https://facebook.com/",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com/?lang=en",
+  },
+  {
+    icon: <FaSquarespace />,
+    url: "https://squarespace.com/?lang=en",
+  },
+]
