84a7a16379be8b3f222bed3d657304aba7e57910
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-20 00:09:49 +0530

    [updated] contact form to work with formspree

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,11 @@ const Contact = () => {
     <section className={styles.contact}>
       <Title title="contact" subtitle="us" />
       <div className={styles.center}>
-        <form className={styles.form}>
+        <form
+          className={styles.form}
+          action="https://formspree.io/sarthak.chow@gmail.com"
+          method="POST"
+        >
           <div>
             <label htmlFor="name">name</label>
             <input
