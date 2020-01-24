5da9da9195977fbd3c486c68e5ff9530036daed5
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-24 20:23:19 +0530

    [added] facebook card

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -35,6 +35,14 @@ const SEO = ({ pageTitle }) => {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
+
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
     </Helmet>
   )
 }
