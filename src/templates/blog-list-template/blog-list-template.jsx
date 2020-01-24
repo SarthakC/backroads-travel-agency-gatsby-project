0d373bd2888a4da6d3828bb5460793d9335a2cb8
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-01-24 18:16:10 +0530

    [updated] bloglist with numbering

--- src/templates/blog-list-template/blog-list-template.jsx
+++ src/templates/blog-list-template/blog-list-template.jsx
@@ -4,6 +4,7 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 import Title from "../../components/Title/Title"
 import BlogCard from "../../components/Blog/BlogCard/BlogCard"
+import Layout from "../../components/Layout/Layout"
 
 import styles from "../../components/Blog/BlogList/blog.module.css"
 
@@ -35,16 +36,37 @@ const BlogList = ({
   data: {
     posts: { edges },
   },
+  pageContext: { currentPage, numPages },
 }) => {
   return (
-    <section className={styles.blog}>
-      <Title title="latest" subtitle="posts" />
-      <div className={styles.center}>
-        {edges.map(({ node }) => {
-          return <BlogCard key={node.id} blog={node} />
-        })}
-      </div>
-    </section>
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {edges.map(({ node }) => {
+            return <BlogCard key={node.id} blog={node} />
+          })}
+        </div>
+        <section className={styles.links}>
+          {Array.from({ length: numPages }, (_, i) => {
+            return (
+              <AniLink
+                fade
+                to={`/blogs/${i === 0 ? "" : i + 1}`}
+                className={
+                  i + 1 === currentPage
+                    ? `${styles.link} ${styles.active}`
+                    : `${styles.link}`
+                }
+                key={i}
+              >
+                {i + 1}
+              </AniLink>
+            )
+          })}
+        </section>
+      </section>
+    </Layout>
   )
 }
 
